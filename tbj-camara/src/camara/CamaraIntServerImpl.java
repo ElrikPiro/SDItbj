b38a39557a7038a767ad74516eb3a88eb38f04c7
David Baselga Masiá <dabama@inf.upv.es>
2016-11-01 11:25:47 +0100

    Se añaden los ficheros fuente de los otros proyectos y se importan los métodos abstractos.

---
+++ tbj-camara/src/camara/CamaraIntServerImpl.java
@@ -0,0 +1,180 @@
+package camara;
+
+import corba.instantanea.*;
+import corba.khepera.escenario.EscenarioD;
+import corba.camara.*;
+import corba.robot.*;
+import java.util.LinkedList;
+
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+
+import java.util.Iterator;
+
+public class CamaraIntServerImpl extends corba.camara.CamaraIntPOA {
+
+   private org.omg.PortableServer.POA poa_;
+   private org.omg.CORBA.ORB orb_;
+
+   private LinkedList listaRobots = new LinkedList();
+   private LinkedList listaEstados = new LinkedList();
+   InstantaneaD instantanea;
+   private int nrobots;
+   private IPYPortD ipyport;
+
+    public
+
+    CamaraIntServerImpl(org.omg.CORBA.ORB orb, org.omg.PortableServer.POA poa, IPYPortD iport) 
+    {
+        orb_ = orb;
+        poa_ = poa;
+        ipyport = new IPYPortD(iport.ip, iport.port);
+        
+        nrobots = 0;
+    }
+
+
+    public org.omg.PortableServer.POA
+    _default_POA()
+    {
+        if(poa_ != null)
+            return poa_;
+        else
+            return super._default_POA();
+    }
+
+    //
+    // IDL:corba/Camara/CamaraInt/SuscribirRobot:1.0
+    //
+    @SuppressWarnings("unchecked")
+	public suscripcionD SuscribirRobot(String IORrob)
+    {
+    	suscripcionD ret = null;
+    	int indexRobot = listaRobots.indexOf(IORrob);
+    	if(indexRobot==-1) {listaRobots.add(IORrob);indexRobot=listaRobots.indexOf(IORrob);}
+    	//ret = new suscripcionD(indexRobot,ipyport, null);
+    	return ret;
+       
+
+    }
+    
+    public void start(){
+        new CamaraDifusion(ipyport).start();
+    }
+
+    //------------------------------------------------------------------------------
+    // La clase anidada CamaraDifusion
+    //------------------------------------------------------------------------------
+    class CamaraDifusion extends Thread{
+     //private Difusion difusion;
+     
+      //------------------------------------------------------------------------------
+      public CamaraDifusion(IPYPortD iport){
+         //difusion = new Difusion(iport);
+      }
+
+      //------------------------------------------------------------------------------
+      public void run(){
+        corba.instantanea.EstadoRobotDHolder st = new EstadoRobotDHolder();
+        String ior=null;
+        LinkedList listaFallos = new LinkedList();
+
+         while(true){
+           listaEstados.clear();
+           listaFallos.clear();
+           Object i[] = listaRobots.toArray();
+           for (int j=0; j<i.length;j++ ){
+             try {
+                //EJERCICIO: invocar via CORBA el metodo ObtenerEstado y anyadir
+               //el estado del robot correspondiente a la lista de estados          
+            	 ior = (String) i[j];
+            	 org.omg.CORBA.Object ncobj=orb_.resolve_initial_references("NameService");
+     			 NamingContextExt nc = NamingContextExtHelper.narrow(ncobj);
+    			 org.omg.CORBA.Object obj = nc.resolve_str(ior);
+    			 
+     			 RobotSeguidorInt status = corba.robot.RobotSeguidorIntHelper.narrow(obj);
+     			 status.ObtenerEstado(st);
+     			 listaEstados.add(st.value);
+     			 
+             } catch (Exception  e){
+                 System.out.println("Detectado fallo Robot: " + ior );
+                 listaFallos.add(ior);
+               
+            } 
+          }
+           
+           Object i1[] = listaFallos.toArray();
+           for (int j=0; j<i1.length;j++ )
+               listaRobots.remove(i1[j]);
+           
+           //EJERCICIO: crear una instantanea a partir de la lista de estados de los robots. 
+           instantanea = new InstantaneaD((EstadoRobotD[]) listaEstados.toArray(new EstadoRobotD[0])); 
+           
+           //EJERCICIO: difundir la instantanea 
+           //Difusion dif = new Difusion(ipyport);
+           //dif.sendObject(instantanea);
+           
+           try{
+               Thread.sleep(400);
+           }catch(InterruptedException e){
+               e.printStackTrace();
+           }
+        }
+      }
+    }
+
+	@Override
+	public void BajaConsola(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void BajaRobot(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void ModificarEscenario(EscenarioD arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public EscenarioD ObtenerEscenario() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public IPYPortD ObtenerIPYPortDifusion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public InstantaneaD ObtenerInstantanea() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public ListaSuscripcionD ObtenerLista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public suscripcionD SuscribirConsola(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ tbj-robot/src/robot/Server_AOM.java
@@ -0,0 +1,113 @@
+package robot;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.sql.Time;
+import java.util.Properties;
+
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContext;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextHelper;
+import org.omg.PortableServer.IdAssignmentPolicyValue;
+// import org.omg.PortableServer.LifespanPolicyValue;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.ThreadPolicyValue;
+/*MODIFICADO*/
+import corba.camara.*;
+/*FIN MODIFICADO*/
+
+public class Server_AOM {
+	  /*MODIFICADO*/	
+	  static CamaraInt camara;
+	  static int ok=0;
+	  /*FIN MODIFICADO*/
+	public static void main(String[] args) {
+
+		Properties props = System.getProperties();
+		props.setProperty("org.omg.CORBA.ORBClass", "com.sun.corba.se.internal.POA.POAORB");
+		props.setProperty("org.omg.CORBA.ORBSingletonClass", "com.sun.corba.se.internal.corba.ORBSingleton");
+		// Solo si se cambia el host 
+		props.put("org.omg.CORBA.ORBInitialHost", "localhost");
+		// Solo si se cambia el port 
+		props.put("org.omg.CORBA.ORBInitialPort", "1050");
+		try {
+			// Initialize the ORB.
+			org.omg.CORBA.ORB orb = org.omg.CORBA.ORB.init(args, props);
+
+			// get a reference to the root POA
+			org.omg.CORBA.Object obj = orb.resolve_initial_references("RootPOA");
+			POA poaRoot = POAHelper.narrow(obj);
+
+			// Create policies for our persistent POA
+			org.omg.CORBA.Policy[] policies = {
+					// poaRoot.create_lifespan_policy(LifespanPolicyValue.PERSISTENT),
+					poaRoot.create_id_assignment_policy(IdAssignmentPolicyValue.USER_ID),
+					poaRoot.create_thread_policy(ThreadPolicyValue.ORB_CTRL_MODEL) 
+			};
+
+			// Create myPOA with the right policies
+			POA poa = poaRoot.create_POA("RobotSeguidorIntServerImpl_poa",	poaRoot.the_POAManager(), policies);
+
+			// Create the servant
+			RobotSeguidorIntServerImpl servant = new RobotSeguidorIntServerImpl();
+
+			// Activate the servant with the ID on myPOA
+			byte[] objectId = "Robot".getBytes();
+			poa.activate_object_with_id(objectId, servant);
+			
+			// Activate the POA manager
+			poaRoot.the_POAManager().activate();
+
+			// Get a reference to the servant and write it down.
+			obj = poa.servant_to_reference(servant);
+
+			// ---- Uncomment below to enable Naming Service access. ----
+			 org.omg.CORBA.Object ncobj = orb.resolve_initial_references("NameService");
+			 NamingContextExt nc = NamingContextExtHelper.narrow(ncobj);
+
+			//PrintWriter ps = new PrintWriter(new FileOutputStream(new File("server.ior")));
+			//ps.println(orb.object_to_string(obj));
+			//ps.close();
+
+			/*MODIFICADO*/			
+		    do{
+		        try{
+		        	//EJERCICIO:Conectar con el servidor de nombre y obtener una referencia 
+		        	//a la **camara*
+		        	org.omg.CORBA.Object camobj = nc.resolve_str("Camara");
+					camara = CamaraIntHelper.narrow(camobj);
+
+		          System.out.println("Identificador: " + servant.toString());
+			         //EJERCICIO: convertir la referencia al robot en un IOR en formato String 
+		         
+
+		          servant.orb = orb;
+		          servant.camara = camara;
+		          if (args.length>0) servant.minombre = args[0]; else servant.minombre="Robot";
+		          servant.miIOR = servant.minombre+Math.random();
+		          nc.rebind(nc.to_name(servant.miIOR), obj);
+		          ok=1;
+		        } catch(Exception ex) {
+		          System.out.println("El robot no se registro bien en la camara. Reintentando...");
+		          ex.printStackTrace();
+		          //System.exit(0);
+		        }
+		      } while(ok==0);
+
+		      servant.start();
+			  /*FIN MODIFICADO*/
+	
+			
+			System.out.println("CORBA Server ready...");
+
+			// Wait for incoming requests
+			orb.run();
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
